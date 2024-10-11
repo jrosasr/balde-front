c9f566e6a8d844fcbb436cb8263e33aae3bb0603
Juan Rosas <juan.rosasr01@gmail.com>
2024-10-11 15:09:44 -0400

    install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+
   runtimeConfig: {
     public: {
       API_URL: process.env.API_URL
     }
   },
+
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "nuxt-lucide-icons"]
+  modules: ['@nuxt/ui', 'nuxt-lucide-icons', '@pinia/nuxt'],
+  compatibilityDate: '2024-10-11'
 })
