fb6f2415fe6ce621944ac7f1787307fc32059b27
Juan Rosas <juan.rosasr01@gmail.com>
2024-10-13 00:43:58 -0400

    responsive header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="max-w-7xl mx-auto p-4">
-    <div class="flex justify-between">
+  <div class="mx-auto p-4 max-w-7xl">
+    <div
+      class="flex flex-wrap md:flex-nowrap justify-center md:justify-between items-center space-y-4"
+    >
       <h1 class="text-2xl">
-        {{ auth.isLoggedIn ? auth.user.full_name : '' }}
-        <span v-if="auth.isLoggedIn" class="text-gray-500">({{ auth.isAdmin ? 'Admin' : 'Reviewer' }})</span>
+        {{ auth.isLoggedIn ? auth.user.full_name : "" }}
+        <span v-if="auth.isLoggedIn" class="text-gray-500"
+          >({{ auth.isAdmin ? "Admin" : "Reviewer" }})</span
+        >
       </h1>
-      <div class="flex space-x-2">
+
+      <div class="flex gap-x-4" style="margin-top: 0">
         <ColorScheme>
-        <USelect
-          v-model="$colorMode.preference"
-          :options="['system', 'light', 'dark']"
-        />
-      </ColorScheme>
-      <UButton v-if="auth.isLoggedIn" @click="logout">
-        Logout
-        <UIcon name="i-lucide-log-out" />
-      </UButton>
+          <USelect
+            v-model="$colorMode.preference"
+            :options="['system', 'light', 'dark']"
+          />
+        </ColorScheme>
+        <UButton v-if="auth.isLoggedIn" @click="logout">
+          Logout
+          <UIcon name="i-lucide-log-out" />
+        </UButton>
       </div>
     </div>
     <slot />
@@ -25,13 +30,12 @@
 
 <script setup>
 import { useApiFetch } from "~/composables/useApiFetch";
-const toast =  useToast();
+const toast = useToast();
 const auth = useAuthStore();
 
 const logout = async () => {
   await auth.logout();
-  
-  toast.add({ title: "Sesión finalizada.", timeout: 2000 });
 
-}
+  toast.add({ title: "Sesión finalizada.", timeout: 2000 });
+};
 </script>
